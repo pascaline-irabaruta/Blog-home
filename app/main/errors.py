25f253fa3b72654e8d5d1d08577305a7b169c78f
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-12-05 10:28:34 +0200

    created a login user view form

--- app/main/errors.py
+++ app/main/errors.py
@@ -13,6 +13,5 @@ def notfound(error):
         new_sub = Subscribers(email = request.form.get("subscriber"))
         db.session.add(new_sub)
         db.session.commit()
-        welcome_message("Thank you for subscribing to the Blog",
-                        "email/welcome", new_sub.email)
+        welcome_message("Thank you for subscribing to the Blog","email/welcome", new_sub.email)
     return render_template("notfound.html"),404
