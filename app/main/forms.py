c5b4fa99f6aa637826341a5aefb3b0218e486181
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-12-05 10:14:12 +0200

    created a form to writw a post

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,20 +7,3 @@ class PostForm(FlaskForm):
     title = StringField("Blog title:", validators=[Required()])
     post = TextAreaField("Type Away:", validators=[Required()])
     submit = SubmitField("Post")
-
-class UpdatePostForm(FlaskForm):
-    title = StringField("Blog title", validators=[Required()])
-    post = TextAreaField("Type Away", validators=[Required()])
-    submit = SubmitField("Update")
-
-class CommentForm(FlaskForm):
-    comment = TextAreaField("Post Comment", validators=[Required()])
-    alias = StringField("Comment Alias")
-    submit = SubmitField("Comment")
-
-class UpdateProfile(FlaskForm):
-    first_name = StringField("First name")
-    last_name = StringField("Last Name")
-    bio = TextAreaField("Bio")
-    email = StringField("Email")
-    submit = SubmitField("Update")
