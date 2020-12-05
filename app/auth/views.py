672fac54a361d962bddb496d2bd91b1fe7e21535
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-12-05 10:11:45 +0200

    created a function to log out the user

--- app/auth/views.py
+++ app/auth/views.py
@@ -31,6 +31,10 @@ def login():
             return redirect(request.args.get("next") or url_for("main.index"))
 
         flash("Invalid Username or Password")
-
     title = "Loging in ..."
     return render_template("auth/login.html",login_form = login_form,title = title)
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
