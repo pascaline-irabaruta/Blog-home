2ac1a23100397f7eaddc1912adef5d77f758917f
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-12-05 10:00:09 +0200

    created a login form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -14,12 +14,17 @@ class SignUpForm(FlaskForm):
     password_confirm = PasswordField("Confirm Password", validators=[Required()])
     submit = SubmitField("Sign Up")
 
-    #Custom email validation
+    # email validation
     def validate_email(self, data_field):
         if User.query.filter_by(email = data_field.data).first():
             raise ValidationError("There is an account with that email")
 
-    #Custom username validation
+    #username validation
     def validate_username(self, data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError("That username is taken")
+class LoginForm(FlaskForm):
+    email = StringField("Your Email Address", validators=[Required(), Email()])
+    password = PasswordField("Password", validators=[Required()])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
