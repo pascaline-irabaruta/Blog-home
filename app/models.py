6e1de1ce7abb4ae4c19fad2437091c9f6fca6de0
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-12-05 09:46:16 +0200

    created class model for postlikes

--- app/models.py
+++ app/models.py
@@ -113,3 +113,10 @@ class Subscribers(db.Model):
     __tablename__ = "subscribers"
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(255), unique = True, index = True)
+
+
+class PostLike(db.Model):
+    __tablename__ = "post_like"
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
